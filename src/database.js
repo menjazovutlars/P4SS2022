47aa23d60567723e4782aed316c496d643f62f96
larsibarsi <58939896+menjazovutlars@users.noreply.github.com>
2022-07-11 17:07:45 +0200

    updated database
    changed database according to database diagramm on miro board

--- src/database.js
+++ src/database.js
@@ -1,21 +1,55 @@
 export const db = {
-  members: [
+  categories: [
     {
       id: 1,
-      name: "Lars",
-      picture_url: "memberImgs/karotte.jpg",
+      name: "vegan",
     },
     {
       id: 2,
-      name: "Elli",
-      picture_url: "memberImgs/kartoffeln.jpg",
+      name: "vegetarian",
     },
     {
       id: 3,
-      name: "Leander",
-      picture_url: "memberImgs/knoblauch.jpg",
+      name: "lowcarb",
+    },
+    {
+      id: 4,
+      name: "meat",
+    },
+    {
+      id: 5,
+      name: "chicken",
+    },
+    {
+      id: 6,
+      name: "vegetable",
+    },
+    {
+      id: 7,
+      name: "fruit",
+    },
+    {
+      id: 8,
+      name: "nut",
+    },
+    {
+      id: 9,
+      name: "sweet",
+    },
+    {
+      id: 11,
+      name: "animalproduct",
+    },
+    {
+      id: 12,
+      name: "highfat",
+    },
+    {
+      id: 13,
+      name: "glutenfree",
     },
   ],
+
   ingredients: [
     {
       id: 1,
@@ -53,27 +87,33 @@ export const db = {
       name: "Mein Lieblingsessen YUMMY",
     },
   ],
-  
-  meals_ingredients:
 
-  [
+  meals_ingredients: [
     {
-        ingredients_id: [1,2,3,4], 
-        meals_id: 1
+      ingredients_id: [1, 2, 3, 4],
+      meals_id: 1,
     },
     {
-        ingredients_id: [1,3],
-        meals_id: 2
+      ingredients_id: [1, 3],
+      meals_id: 2,
     },
     {
-        ingredients_id: [2,3],
-        meals_id: 3
+      ingredients_id: [2, 3],
+      meals_id: 3,
     },
     {
-        ingredients_id: [1],
-        meals_id: 4
-    }
-],
+      ingredients_id: [1],
+      meals_id: 4,
+    },
+    {
+      ingredients_id: [3, 8],
+      meals_id: 5,
+    },
+    {
+      ingredients_id: [1, 2, 4, 7],
+      meals_id: 6,
+    },
+  ],
 
   meals_steps: [
     {
@@ -94,18 +134,78 @@ export const db = {
     },
   ],
 
-  members_meals: [
+  members: [
+    {
+      id: 1,
+      name: "Lars",
+      picture_url: "memberImgs/karotte.jpg",
+    },
+    {
+      id: 2,
+      name: "Elli",
+      picture_url: "memberImgs/kartoffeln.jpg",
+    },
+    {
+      id: 3,
+      name: "Leander",
+      picture_url: "memberImgs/knoblauch.jpg",
+    },
+  ],
+
+  members_needs: [
     {
       members_id: 1,
-      meals_id: [1,3],
+      needs_id: 1,
     },
     {
       members_id: 2,
-      meals_id: [2,4],
+      needs_id: 5,
     },
     {
       members_id: 3,
-      meals_id: [2],
+      needs_id: 2,
+    },
+  ],
+
+  needs: [
+    {
+      id: 1,
+      name: "vegan",
+    },
+    {
+      id: 2,
+      name: "keto diet",
+    },
+    {
+      id: 3,
+      name: "weightloss",
+    },
+    {
+      id: 4,
+      name: "pregnant",
+    },
+    {
+      id: 5,
+      name: "meateater",
+    },
+    {
+      id: 6,
+      name: "gluten allergy",
+    },
+  ],
+
+  needs_categories: [
+    {
+      categories_id: [1,6,7],
+      needs_id: 1,
+    },
+    {
+      categories_id: [3,8,11,12],
+      needs_id: 2,
+    },
+    {
+      categories_id: [4,5,11],
+      needs_id: 5,
     },
   ],
 
